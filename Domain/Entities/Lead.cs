c1c682a84e0fdc1c819bfd439069011a526f5967
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-09-06 18:39:49 -0300

    feat: update entity

--- Domain/Entities/Lead.cs
+++ Domain/Entities/Lead.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace dotnet_backend.Domain.Entities
 {
     //clasificacion de cliente (frio, tibio, caliente) as enum.
@@ -32,47 +34,26 @@ namespace dotnet_backend.Domain.Entities
         // TODO: use Guid as primary key instead of int Id but keep Id for now.
         public Guid Uuid { get; set; } = Guid.NewGuid();
         public int Id { get; set; }
-        //Rut
         public string Rut { get; set; } = null!;
-        // DV
         public string Dv { get; set; } = null!;
-        //primer nombre
         public string PrimerNombre { get; set; } = null!;
-        //segundo nombre
         public string SegundoNombre { get; set; } = null!;
-        //primer apellido
         public string PrimerApellido { get; set; } = null!;
-        //segundo apellido
         public string SegundoApellido { get; set; } = null!;
-        //email
         public string Email { get; set; } = null!;
-        //tel movil
         public string TelMovil { get; set; } = null!;
-        //canal registro // canal de registro
         public string CanalRegistro { get; set; } = null!;
-        //sucursal // sucursal de preferencia // should be a foreign key to Sucursal
         public string Sucursal { get; set; } = null!;
-        //clasificacion // clasificacion de cliente (frio, tibio, caliente)
         public Clasificacion Clasificacion { get; set; } = Clasificacion.Frio;
-        //estado // estado del cliente (sin gestion, contactado, no contactado, no interesado, interesado)
         public Estado Estado { get; set; } = Estado.SinGestion;
-        //tipo lead // tipo de lead (empresa, pensionado, trabajador, independiente)
         public TipoLead TipoLead { get; set; } = TipoLead.Trabajador;
-        //fch registro // fecha de registro
         public DateTime FchRegistro { get; set; } = DateTime.Now;
-        //prioridad // priohttps://github.com/chrisloarryn/dotnet-sql-server-cleanridad de contacto
         public string Prioridad { get; set; } = null!;
-        //ejecutivo asignado // ejecutivo de incorporacion asignado
         public string EjecutivoAsignado { get; set; } = null!;
         
-        // adicionalmente
-        //direccion
         public string Direccion { get; set; } = null!;
-        //ciudad
         public string Ciudad { get; set; } = null!;
-        //comuna
         public string Comuna { get; set; } = null!;
-        //region
         public string Region { get; set; } = null!;
     }
 }
---
+++ Migrations/20230906174129_LeadMigration.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class LeadMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Leads",
+                newName: "TelMovil");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CanalRegistro",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ciudad",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Clasificacion",
+                table: "Leads",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comuna",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Direccion",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dv",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EjecutivoAsignado",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Estado",
+                table: "Leads",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FchRegistro",
+                table: "Leads",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PrimerApellido",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PrimerNombre",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Prioridad",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rut",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SegundoApellido",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SegundoNombre",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sucursal",
+                table: "Leads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TipoLead",
+                table: "Leads",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Uuid",
+                table: "Leads",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanalRegistro",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Ciudad",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Clasificacion",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Comuna",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Direccion",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Dv",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "EjecutivoAsignado",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "FchRegistro",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "PrimerApellido",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "PrimerNombre",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Prioridad",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Rut",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "SegundoApellido",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "SegundoNombre",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Sucursal",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "TipoLead",
+                table: "Leads");
+
+            migrationBuilder.DropColumn(
+                name: "Uuid",
+                table: "Leads");
+
+            migrationBuilder.RenameColumn(
+                name: "TelMovil",
+                table: "Leads",
+                newName: "Name");
+        }
+    }
+}
